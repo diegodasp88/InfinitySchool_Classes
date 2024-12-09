47a8aa0e1eb3cf613c776dea93ec68804b27893b
diegodasp88 <167533683+diegodasp88@users.noreply.github.com>
2024-12-09 19:48:42 -0300

    aula05_exercicio02.py
    Atualização do código após a correção do exercício em sala.

--- Python/Aula 05/aula05_exercicio02.py
+++ Python/Aula 05/aula05_exercicio02.py
@@ -1,7 +1,11 @@
-'''Ex 02. Faça uma função chamada "verificar_par" que recebe um numero e retorna um booleano (True se for par, False se for impar)'''
+'''Ex 02. Faça uma função chamada "verificar_par" que recebe um número inteiro e retorna um booleano (True se for par, False se for impar)'''
 
     # Definindo a função
-def verificar_par(num):
+def verificar_par(num: int) -> bool:
+    """Função que verifica se o número é par ou não
+        Arg: num (int)
+        Returns: (bool)
+    """
     if num % 2 == 0:
         return True
     else:
