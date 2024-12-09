b12295c45d358bfdb12b78bea8d461da680a3c71
diegodasp88 <167533683+diegodasp88@users.noreply.github.com>
2024-12-09 20:54:04 -0300

    aula05_exercicio03.py
    Atualização após correção de exercícios na sala de aula.

--- Python/Aula 05/aula05_exercicio03.py
+++ Python/Aula 05/aula05_exercicio03.py
@@ -9,20 +9,38 @@ Nomes:
 - Lidia
 '''
 
-    # Definindo a função
-def listar_nomes(nomes:list) -> str:
-    'Essa função serve para mostrar os nomes cada um em uma linha'
-    for nome in nomes:
-        print(f' - {nome}')
-
 nomes = []
-cont = 's'
 
-    # ENTRADA: Coletando os nomes pelo usuário:
-while cont.lower() == 's':
-    nome = input('Digite um nome: ')
-    nomes.append(nome)
-    cont = input('Você deseja digitar mais um nome (s/n)? ')
+# Definindo a função de listar
+def listar_nomes(nomes: list):
+    print('Nomes: ')
+    for nome in nomes:
+        print(f'- {nome}')
 
-    # SAÍDA: Chamando a função para mostrar a lista de nomes
-listar_nomes(nomes)
+# Definindo a função de adicionar
+def adicionar_nome(dados: list):
+    nome = input('Digite o nome: ')
+    dados.append(nome)
+
+# Menu
+while True:
+    print(' Armazenador de Nomes '.center(30, '-'))
+    print('[1] - Listar Nomes')
+    print('[2] - Adicionar novo Nome')
+    print('[3] - Sair')
+    print('-' * 30)
+
+    opcao = input('Digite uma opção: ')
+
+# Acessando as opções
+    if opcao == '1':
+        listar_nomes(nomes)
+    elif opcao == '2':
+        adicionar_nome(nomes)
+    elif opcao == '3':
+        print('Saindo do programa...')
+        break
+    else:
+        print('Opção Inválida. Digite Novamente.')
+
+print('Fim do Programa.')
