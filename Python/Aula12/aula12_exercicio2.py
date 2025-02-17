d4c1bece43cc4e4590d319df8a57cb8ab2ee811c
Diego De Angelis <167533683+diegodasp88@users.noreply.github.com>
2025-02-17 19:59:17 -0300

    Update aula12_exercicio2.py
    Update após correção.

--- Python/Aula12/aula12_exercicio2.py
+++ Python/Aula12/aula12_exercicio2.py
@@ -1,30 +1,38 @@
-# Ex02. Crie uma classe "Imposto" que deve ter um metódo "calcular(valor: float)", que retornará o proprio valor.
+# Ex02. Crie uma classe "Imposto" que deve ter um metódo "calcular(valor: float)", que retornará o 0.
 
 class Imposto:
     def calcular(self, valor: float):
-        return valor
-
-# a. Crie uma classe ISS, que irá herdar de "Imposto", mas o metódo "calcular(valor: float)" deve retornar o valor do imposto com uma dedução de 6%
-class ISS(Imposto):
+        return 0
+    
+# a. Crie uma classe ISS, que irá herdar de "Imposto", mas o metódo "calcular(valor: float)" deve retornar 6% do valor
+class Iss(Imposto):
     def calcular(self, valor: float):
-        imposto = 0.06
-        return valor * (1 - imposto)
-
-# b. Crie uma outra classe ICMS, que irá herdar de "Imposto", mas o método "calcular(valor: float)" deve retornar o valor do imposto com uma dedução de 7%
-class ICMS(Imposto):
+        taxa = 0.06
+        return valor * taxa
+    
+# b. Crie uma outra classe ICMS, que irá herdar de "Imposto", mas o metódo "calcular(valor: float)" deve retornar 4% do valor
+class Icms(Imposto):
     def calcular(self, valor: float):
-        imposto = 0.07
-        return valor * (1 - imposto)
+        taxa = 0.04
+        return valor * taxa
+    
 
 # c. Crie uma função chamada "calcular_impostos" fora das classes, que irá receber o valor, e uma lista com dois objetos das classes "ISS" e "ICMS" respectivamente.
-def calcular_impostos(valor: float, impostos: list = [ISS, ICMS]):
+def calcular_imposto(
+    valor: float, impostos: list[Imposto]
+) -> float:
+    total_imposto = 0
+
     for imposto in impostos:
-        print(f'{imposto.__class__.__name__}: R${imposto.calcular(valor):.2f}')
+        total_imposto += imposto.calcular(valor)
 
-# Criando instâncias das classes
-iss = ISS()
-icms = ICMS()
-valor = float(input('Digite o valor: R$'))
+    return valor - total_imposto
 
-# Chamando a função com os impostos
-calcular_impostos(valor, [iss, icms])
+
+valor_bruto = float(input('Digite o valor da nota: '))
+
+impostos = [Iss(), Icms()]
+valor_liquido = calcular_imposto(valor_bruto, impostos)
+
+print(f'Valor da Nota Bruto: {valor_bruto}')
+print(f'Valor da Nota Liquido: {valor_liquido}')
