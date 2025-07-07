18bccd98fafddd2560bb9c68ca015ff389a7094f
Diego De Angelis <167533683+diegodasp88@users.noreply.github.com>
2025-07-07 19:50:30 -0300

    Add files via upload

---
+++ JavaScript/Aula 03/script.js
@@ -0,0 +1,25 @@
+// Faça um contador que inicie em 10 e vá até 100 de 2 em 2.
+// for(let i = 10; i < 100+1; i += 2){
+//     console.log(i)
+// }
+
+// Faça um contador que inicie em 50 e vá até 100 de 2 em 2.
+// for(let i = 50; i < 100+1; i += 2){
+//     console.log(i)
+// }
+
+// Faça um contador que inicie em 100 e vá até 1.
+// for(let i = 100; i > 0; i -= 1){
+//     console.log(i)
+// }
+
+// Itere um nome para que percorra cada letra sua. 
+// Transforme as letras em maiúsculas e minúsculas.
+// let nome = "Osvaldo"
+// for (letra of nome){
+//     console.log(letra)
+//     //console.log(letra.toUpperCase())
+//     //console.log(letra.toLowerCase())
+// }
+
+//
